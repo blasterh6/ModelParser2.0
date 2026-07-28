ed2df47c1c3bd5341d313a57520df763fbf894fa
Ruben Ramirez <ruben.ramirez@protector.com.mx>
2026-07-28 10:38:04 -0600

    Initial commit for ModelParser 2.0

--- ModelParser/Form1.Designer.vb
+++ ModelParser/Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,16 +22,340 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        GroupBox1 = New GroupBox()
+        CheckBox1 = New CheckBox()
+        GroupBox4 = New GroupBox()
+        ListBox1 = New ListBox()
+        Label11 = New Label()
+        Button3 = New Button()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
+        Label10 = New Label()
+        Label9 = New Label()
+        TableLayoutPanel2 = New TableLayoutPanel()
+        Button1 = New Button()
+        Button2 = New Button()
+        GroupBox3 = New GroupBox()
+        RichTextBox1 = New RichTextBox()
+        SaveFileDialog1 = New SaveFileDialog()
+        OpenFileDialog1 = New OpenFileDialog()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        TableLayoutPanel1.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        TableLayoutPanel2.SuspendLayout()
+        GroupBox3.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 76.8278961F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 23.1721039F))
+        TableLayoutPanel1.Controls.Add(GroupBox1, 1, 1)
+        TableLayoutPanel1.Controls.Add(GroupBox2, 0, 0)
+        TableLayoutPanel1.Controls.Add(TableLayoutPanel2, 1, 0)
+        TableLayoutPanel1.Controls.Add(GroupBox3, 0, 1)
+        TableLayoutPanel1.Dock = DockStyle.Fill
+        TableLayoutPanel1.Location = New Point(0, 0)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 2
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 14.7766323F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 85.2233658F))
+        TableLayoutPanel1.Size = New Size(889, 582)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Controls.Add(GroupBox4)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Dock = DockStyle.Fill
+        GroupBox1.Location = New Point(686, 89)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 490)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Info Panel"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Checked = True
+        CheckBox1.CheckState = CheckState.Checked
+        CheckBox1.Location = New Point(6, 223)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(114, 19)
+        CheckBox1.TabIndex = 9
+        CheckBox1.Text = "Modo Silencioso"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.Controls.Add(ListBox1)
+        GroupBox4.Controls.Add(Label11)
+        GroupBox4.Controls.Add(Button3)
+        GroupBox4.Dock = DockStyle.Bottom
+        GroupBox4.Location = New Point(3, 248)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(194, 239)
+        GroupBox4.TabIndex = 8
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "Archivos en Serie"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Dock = DockStyle.Bottom
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(3, 82)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(188, 154)
+        ListBox1.TabIndex = 2
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(6, 52)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(64, 15)
+        Label11.TabIndex = 1
+        Label11.Text = "Ruta e info"
+        ' 
+        ' Button3
+        ' 
+        Button3.Dock = DockStyle.Top
+        Button3.Location = New Point(3, 19)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(188, 30)
+        Button3.TabIndex = 0
+        Button3.Text = "Seleccionar Carpeta"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(7, 194)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(55, 15)
+        Label5.TabIndex = 7
+        Label5.Text = "Esquema"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(7, 170)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(54, 15)
+        Label6.TabIndex = 6
+        Label6.Text = "Almacen"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(7, 145)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(62, 15)
+        Label7.TabIndex = 5
+        Label7.Text = "Solicitante"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(7, 121)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(84, 15)
+        Label8.TabIndex = 4
+        Label8.Text = "Observaciones"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(6, 95)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(51, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Moneda"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(6, 71)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(38, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "Fecha"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(6, 46)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(61, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Proveedor"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Orden"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Label10)
+        GroupBox2.Controls.Add(Label9)
+        GroupBox2.Dock = DockStyle.Fill
+        GroupBox2.Location = New Point(3, 3)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(677, 80)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Origen - Destino"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(20, 50)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(47, 15)
+        Label10.TabIndex = 1
+        Label10.Text = "Destino"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(19, 26)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(48, 15)
+        Label9.TabIndex = 0
+        Label9.Text = "Archivo"
+        ' 
+        ' TableLayoutPanel2
+        ' 
+        TableLayoutPanel2.ColumnCount = 1
+        TableLayoutPanel2.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel2.Controls.Add(Button1, 0, 0)
+        TableLayoutPanel2.Controls.Add(Button2, 0, 1)
+        TableLayoutPanel2.Location = New Point(686, 3)
+        TableLayoutPanel2.Name = "TableLayoutPanel2"
+        TableLayoutPanel2.RowCount = 2
+        TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel2.Size = New Size(200, 76)
+        TableLayoutPanel2.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Dock = DockStyle.Fill
+        Button1.Location = New Point(3, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(194, 32)
+        Button1.TabIndex = 0
+        Button1.Text = "Cargar Archivo"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Dock = DockStyle.Fill
+        Button2.Location = New Point(3, 41)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(194, 32)
+        Button2.TabIndex = 1
+        Button2.Text = "Guardar Modelo"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(RichTextBox1)
+        GroupBox3.Dock = DockStyle.Fill
+        GroupBox3.Location = New Point(3, 89)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(677, 490)
+        GroupBox3.TabIndex = 3
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Previa"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Dock = DockStyle.Fill
+        RichTextBox1.Location = New Point(3, 19)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.ReadOnly = True
+        RichTextBox1.Size = New Size(671, 468)
+        RichTextBox1.TabIndex = 0
+        RichTextBox1.Text = ""
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "Abrir CSV"
+        OpenFileDialog1.Filter = "CSV Files (*.csv)|*.csv|All files (*.*)|*.*"
+        OpenFileDialog1.RestoreDirectory = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1280, 706)
+        ClientSize = New Size(889, 582)
+        Controls.Add(TableLayoutPanel1)
         Name = "Form1"
-        Text = "Form1"
+        Text = "Model Parser"
+        TableLayoutPanel1.ResumeLayout(False)
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        TableLayoutPanel2.ResumeLayout(False)
+        GroupBox3.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
 
 End Class
--- ModelParser/Form1.vb
+++ ModelParser/Form1.vb
@@ -1,3 +1,321 @@
-﻿Public Class Form1
+﻿Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Reflection
 
+Public Class Form1
+    Dim datosorden As String
+    Dim claveorden As String
+    'copiar al portapapeles
+    Private Sub copiar(ByVal file As String)
+        If System.IO.File.Exists(file) Then
+            My.Computer.Clipboard.Clear()
+            Dim paths As StringCollection = New StringCollection()
+            paths.Add(file)
+            Clipboard.SetFileDropList(paths)
+            MsgBox("Se copio el modelo al portapapeles: " & file)
+        Else
+            MsgBox("No se encontro el archivo: " & file)
+        End If
+    End Sub
+
+    'iniciar el modelo
+    Private Sub iniciarmodelo()
+        datosorden = Nothing
+        claveorden = Nothing
+        RichTextBox1.Text = ""
+        Label1.Text = "Orden" 'orden de compra
+        Label2.Text = "Proveedor" 'proveedor
+        Label3.Text = "Moneda" 'moneda
+        Label4.Text = "Fecha" 'fecha
+        Label5.Text = "Esquema" 'esquema impuestos
+        Label6.Text = "Almacen" 'almacen
+        Label7.Text = "Solicitante" 'solicitante
+        Label8.Text = "Observaciones" 'observaciones
+        Label9.Text = "Archivo"
+        Label10.Text = "Destino"
+        RichTextBox1.AppendText("<?xml version=""1.0"" standalone=""yes""?>  
+<DATAPACKET Version=""2.0"">
+    <METADATA>
+        <FIELDS>
+            <FIELD attrname=""CVE_CLPV"" fieldtype=""string"" WIDTH=""10""/>
+            <FIELD attrname=""NUM_ALMA"" fieldtype=""i4""/>
+            <FIELD attrname=""CVE_PEDI"" fieldtype=""string"" WIDTH=""20""/>
+            <FIELD attrname=""ESQUEMA"" fieldtype=""i4""/>
+            <FIELD attrname=""DES_TOT"" fieldtype=""r8""/>
+            <FIELD attrname=""DES_FIN"" fieldtype=""r8""/>
+            <FIELD attrname=""CVE_VEND"" fieldtype=""string"" WIDTH=""5""/>
+            <FIELD attrname=""COM_TOT"" fieldtype=""r8""/>
+            <FIELD attrname=""NUM_MONED"" fieldtype=""i4""/>
+            <FIELD attrname=""TIPCAMB"" fieldtype=""r8""/>
+            <FIELD attrname=""STR_OBS"" fieldtype=""string"" WIDTH=""255""/>
+            <FIELD attrname=""ENTREGA"" fieldtype=""string"" WIDTH=""25""/>
+            <FIELD attrname=""SU_REFER"" fieldtype=""string"" WIDTH=""20""/>
+            <FIELD attrname=""TOT_IND"" fieldtype=""r8""/>
+            <FIELD attrname=""MODULO"" fieldtype=""string"" WIDTH=""4""/>
+            <FIELD attrname=""CONDICION"" fieldtype=""string"" WIDTH=""25""/>
+            <FIELD attrname=""dtfield"" fieldtype=""nested"">
+                <FIELDS>
+                    <FIELD attrname=""CANT"" fieldtype=""r8""/>
+                    <FIELD attrname=""CVE_ART"" fieldtype=""string"" WIDTH=""20""/>
+                    <FIELD attrname=""DESC1"" fieldtype=""r8""/>
+                    <FIELD attrname=""DESC2"" fieldtype=""r8""/>
+                    <FIELD attrname=""DESC3"" fieldtype=""r8""/>
+                    <FIELD attrname=""IMPU1"" fieldtype=""r8""/>
+                    <FIELD attrname=""IMPU2"" fieldtype=""r8""/>
+                    <FIELD attrname=""IMPU3"" fieldtype=""r8""/>
+                    <FIELD attrname=""IMPU4"" fieldtype=""r8""/>
+                    <FIELD attrname=""COMI"" fieldtype=""r8""/>
+                    <FIELD attrname=""PREC"" fieldtype=""r8""/>
+                    <FIELD attrname=""NUM_ALM"" fieldtype=""i4""/>
+                    <FIELD attrname=""STR_OBS"" fieldtype=""string"" WIDTH=""255""/>
+                    <FIELD attrname=""REG_GPOPROD"" fieldtype=""i4""/>
+                    <FIELD attrname=""REG_KITPROD"" fieldtype=""i4""/>
+                    <FIELD attrname=""NUM_REG"" fieldtype=""i4""/>
+                    <FIELD attrname=""COSTO"" fieldtype=""r8""/>
+                    <FIELD attrname=""TIPO_PROD"" fieldtype=""string"" WIDTH=""1""/>
+                    <FIELD attrname=""TIPO_ELEM"" fieldtype=""string"" WIDTH=""1""/>
+                    <FIELD attrname=""MINDIRECTO"" fieldtype=""r8""/>
+                    <FIELD attrname=""TIP_CAM"" fieldtype=""r8""/>
+                    <FIELD attrname=""FACT_CONV"" fieldtype=""r8""/>
+                    <FIELD attrname=""UNI_VENTA"" fieldtype=""string"" WIDTH=""10""/>
+                    <FIELD attrname=""IMP1APLA"" fieldtype=""i4""/>
+                    <FIELD attrname=""IMP2APLA"" fieldtype=""i4""/>
+                    <FIELD attrname=""IMP3APLA"" fieldtype=""i4""/>
+                    <FIELD attrname=""IMP4APLA"" fieldtype=""i4""/>
+                    <FIELD attrname=""PREC_SINREDO"" fieldtype=""r8""/>
+                    <FIELD attrname=""COST_SINREDO"" fieldtype=""r8""/>
+                    <FIELD attrname=""LOTE"" fieldtype=""string"" WIDTH=""16""/>
+                    <FIELD attrname=""PEDIMENTO"" fieldtype=""string"" WIDTH=""16""/>
+                    <FIELD attrname=""FECHCADUC"" fieldtype=""dateTime""/>
+                    <FIELD attrname=""FECHADUANA"" fieldtype=""dateTime""/>
+                </FIELDS>
+                <PARAMS/>
+            </FIELD>
+        </FIELDS>
+        <PARAMS/>
+    </METADATA>
+    <ROWDATA>")
+    End Sub
+    'cargar archivo
+    Private Sub cargararchivo(ByVal archivo As String)
+        Dim file As String = archivo
+        Using MyReader As New Microsoft.VisualBasic.
+                      FileIO.TextFieldParser(
+                        file)
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim currentRow As String()
+            Dim cline As Integer = 1
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    'Dim currentField As String 'para debuguear campo por campo
+
+                    If Not currentRow.Length = 12 Then
+                        MsgBox("Formato de importacion incorrecto")
+                        iniciarmodelo()
+                        Exit Sub
+                    End If
+
+                    If cline > 1 Then '1 son los headers, a partir de 2 ya son datos
+                        'agregar directamente segun el indice
+
+                        Label1.Text = Label1.Text + ": " + currentRow(0).ToString() 'orden de compra
+
+
+                        Label2.Text = Label2.Text + ": " + currentRow(1).ToString() 'proveedor
+                        Label3.Text = Label3.Text + ": " + currentRow(3).ToString() 'moneda
+                        Label4.Text = Label4.Text + ": " + currentRow(2).ToString() 'fecha
+                        Label5.Text = Label5.Text + ": " + currentRow(9).ToString() 'esquema impuestos
+                        Label6.Text = Label6.Text + ": 1" 'almacen
+                        Label7.Text = Label7.Text + ": " + currentRow(5).ToString() 'solicitante
+                        Label8.Text = Label8.Text + ": " + currentRow(4).ToString() 'observaciones
+
+
+                        Dim proveedor As String = currentRow(1).ToString() 'proveedor
+                        Dim almacen As String = "1"
+                        Dim esquema As String
+                        If currentRow(9).ToString() = "16.0" Then
+                            esquema = "9"
+                        ElseIf currentRow(9).ToString() = "8.0" Then
+                            esquema = "13"
+                        ElseIf currentRow(9).ToString() = "-4.0" Then
+                            esquema = "14"
+                        Else
+                            esquema = "12"
+                        End If
+                        Dim moneda As String
+                        If currentRow(3).ToString() = "MXN" Then
+                            moneda = "1"
+                        Else
+                            moneda = "2"
+                        End If
+
+                        Dim observaciones As String = currentRow(4).ToString() & " SOLICITADO POR: " & currentRow(5).ToString() 'observaciones
+                        Dim entregaren As String = currentRow(11).ToString() 'entregar en
+
+                        If cline = 2 Then ' aqui nomas se llena una vez la informacion de la orden
+                            claveorden = currentRow(0).ToString()
+                            Label10.Text = "Guardar como: " & claveorden
+                            datosorden = "<ROW 
+                                            CVE_CLPV=""" & proveedor & """ 
+                                            NUM_ALMA=""" & almacen & """ 
+                                            ESQUEMA=""" & esquema & """ 
+                                            DES_TOT=""0"" 
+                                            DES_FIN=""0"" 
+                                            NUM_MONED=""" & moneda & """ 
+                                            TIPCAMB=""1"" 
+                                            STR_OBS=""" & observaciones & """ 
+                                            ENTREGA=""" & entregaren & """ 
+                                            SU_REFER="""" 
+                                            TOT_IND=""0"" 
+                                            MODULO=""COMP"">
+                                            <dtfield>"
+                            RichTextBox1.AppendText(datosorden)
+                        End If
+
+                        'aqui se tienen que llenar las partidas
+                        Dim cant As Integer = currentRow(8).ToString()
+                        Dim prod As String = currentRow(7).ToString()
+                        Dim iva As Integer = 0
+                        If currentRow(9).ToString().Contains(".") Then
+                            iva = currentRow(9)
+                        End If
+                        Dim ret As Integer = 0 'currentRow(9).ToString()
+                        Dim desc As String = currentRow(10).ToString()
+                        Dim costo As String = currentRow(6).ToString()
+
+
+
+
+                        Dim lineaoc As String = "<ROWdtfield 
+                    CANT=""" & cant & """ 
+                    CVE_ART=""" & prod & """ 
+                    DESC1=""0"" 
+                    IMPU1=""0"" 
+                    IMPU2=""0"" 
+                    IMPU3=""" & ret & """ 
+                    IMPU4=""" & iva & """ 
+                    PREC=""0"" 
+                    NUM_ALM=""" & almacen & """ 
+                    STR_OBS=""" & desc & """ 
+                    REG_GPOPROD=""0"" 
+                    COSTO=""" & costo & """ 
+                    TIPO_PROD=""P"" 
+                    TIPO_ELEM=""N"" 
+                    MINDIRECTO=""0"" 
+                    TIP_CAM=""1"" 
+                    FACT_CONV=""1"" 
+                    UNI_VENTA=""pz"" 
+                    IMP1APLA=""6"" 
+                    IMP2APLA=""6"" 
+                    IMP3APLA=""6"" 
+                    IMP4APLA=""1"" 
+                    PREC_SINREDO=""0"" 
+                    COST_SINREDO=""" & costo & """/>"
+
+                        RichTextBox1.AppendText(lineaoc)
+
+                    End If
+
+                    'For Each currentField In currentRow
+                    ' MsgBox(currentField & "index: " & Array.IndexOf(currentRow, currentField))
+                    ' Next
+
+                Catch ex As Microsoft.VisualBasic.
+                            FileIO.MalformedLineException
+                    MsgBox("Linea " & ex.Message &
+                    "no valida, se saltara.")
+                End Try
+                cline = cline + 1
+            End While
+        End Using
+        'cerrar el modelo
+        Dim cierre As String = " </dtfield>
+        </ROW>
+    </ROWDATA>
+</DATAPACKET>"
+        RichTextBox1.AppendText(cierre)
+    End Sub
+    'boton cargar informacion
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim file As String
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            file = OpenFileDialog1.FileName
+            iniciarmodelo()
+            Label9.Text = "Archivo: " & file
+        Else
+            Exit Sub
+        End If
+        If Not IsNothing(file) Then
+            MsgBox(file)
+            cargararchivo(file)
+        End If
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        iniciarmodelo()
+    End Sub
+    'guardar modelo
+    Private Sub guardarmodelo(ByVal modelo As String)
+        Dim sw As New StreamWriter(modelo)
+        sw.Write(RichTextBox1.Text)
+        sw.Close()
+        If CheckBox1.Checked = False Then
+            If MsgBox("Quieres abrir el archivo guardado?", MsgBoxStyle.YesNo, "Modelo Guardado") = MsgBoxResult.Yes Then
+                Try
+                    Process.Start("notepad.exe", modelo)
+                Catch ex As Exception
+                    MsgBox(ex.Message.ToString())
+                End Try
+            End If
+            copiar(modelo)
+        End If
+        iniciarmodelo()
+    End Sub
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.Filter = "TextFile (*.mod)|*.mod"
+        If Not claveorden Is Nothing Then
+            SaveFileDialog1.FileName = Trim(claveorden.ToUpper)
+        End If
+        If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim modelo As String = SaveFileDialog1.FileName
+            guardarmodelo(modelo)
+        End If
+    End Sub
+    Private Sub procesarlote(ByVal folder As String)
+        For Each item In ListBox1.Items()
+            'MsgBox(item)
+            cargararchivo(item)
+            If Not claveorden Is Nothing Then
+
+                Dim model As String = folder & "\" & Trim(claveorden.ToUpper()) & ".mod"
+                'MsgBox(model)
+                guardarmodelo(model)
+            End If
+            'guardarmodelo(item) 'falta parsear el nom,bre con el que se guardara el archivo mod
+        Next
+        MsgBox("Se proceso el lote")
+        iniciarmodelo()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            ListBox1.Items.Clear()
+            Dim f As String = FolderBrowserDialog1.SelectedPath
+            Dim files As String() = Directory.GetFiles(f, "*.csv")
+            For Each file In files
+                ListBox1.Items.Add(file)
+            Next
+            'empezar el cagadero
+            procesarlote(f)
+        End If
+    End Sub
 End Class
